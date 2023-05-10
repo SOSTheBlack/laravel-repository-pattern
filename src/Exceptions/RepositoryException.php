0358a3b6165f932f9b5704a9caf0d09ca76a60f4
SOSTheBlack <jeancesargarcia@gmail.com>
2023-05-10 16:47:45 -0300

    chore: Criado exceptions

--- src/Exceptions/RepositoryException.php
+++ src/Exceptions/RepositoryException.php
@@ -3,14 +3,15 @@
 namespace SOSTheBlack\Repository\Exceptions;
 
 use Exception;
+use Throwable;
 
 /**
  * Class RepositoryException
-
+ *
  * @package SOSTheBlack\Repository\Exceptions
  * @author Jean C. Garcia <garciasoftwares@gmail.com>
  */
-class RepositoryException extends Exception
+class RepositoryException extends Exception implements Throwable
 {
 
 }
